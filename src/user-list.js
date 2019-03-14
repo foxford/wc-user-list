e54005001fd7b760f116911151a134bc3f15a7ed
Aleksey Konstantinov <a.konstantinov@netology-group.ru>
2019-03-14 11:29:44 +0300

    MEDIA-2121 Added 'withActions' mixin

--- src/user-list.js
+++ src/user-list.js
@@ -6,21 +6,17 @@ import groupBy from 'lodash/groupBy'
 
 import style from './user-list.css'
 
-// todo: extract action mixin (canblock)
-
 export class UserListElement extends LitElement {
   static get properties () {
     return {
       users: Array,
-      me: Number,
-      canblock: Boolean
+      me: String
     }
   }
-  _handleLockClick (userId) {
-    this.dispatchEvent(new CustomEvent('toggle-user-lock', {detail: {id: userId}}))
-  }
   _isMe (user, me) {
-    return user.id === me
+    return typeof user.id === 'number'
+      ? user.id === Number(me)
+      : user.id === me
   }
   _selectUsers (props) {
     const { users } = props
@@ -33,40 +29,20 @@ export class UserListElement extends LitElement {
     return html`
       <div class="info">
         <div class="avatar">
-          <img class="image" src="${user.avatar}" alt="${user.name}">
+          ${user.avatar
+            ? html`<img class="image" src="${user.avatar}" alt="${user.name}">`
+            : html`<div class="image"></div>`
+          }
           ${user.online ? html`<div class="status"></div>` : null}
         </div>
         <div class$="${classString({name: true, highlighted: this._isMe(user, me)})}">${user.name}</div>
       </div>
     `
   }
-  _renderLockAction (user, props) {
-    const { me, canblock } = props
-
-    return canblock
-      ? html`
-        <div
-          class$="${classString({lock: true, active: user.blocked})}"
-          on-click="${() => this._handleLockClick(user.id)}"
-        >
-          <div class="popover">${user.blocked ? 'Разблокировать' : 'Заблокировать'}</div>
-        </div>
-      `
-      : user.blocked
-        ? html`
-          <div class="lock active">
-            <div class="popover">${this._isMe(user, me) ? 'Вас заблокировали' : 'Заблокирован'}</div>
-          </div>
-        `
-        : null
-  }
   _renderListItem (user, props) {
     return html`
       <div class="item">
         ${this._renderUserInfo(user, props)}
-        <div class="actions">
-          ${this._renderLockAction(user, props)}
-        </div>
       </div>
     `
   }
@@ -92,6 +68,68 @@ export class UserListElement extends LitElement {
     return html`
       ${this._renderWrapper(props)}
     `
+  }
+}
+
+function withActions(baseClass, config) {
+  return class extends baseClass {
+    static get properties() {
+      const props = {}
+
+      Object.keys(config).forEach((prop) => {
+        props[prop] = Boolean
+      })
+
+      return {
+        ...super.properties,
+        ...props
+      }
+    }
+    _handleActionClick (eventName, data) {
+      this.dispatchEvent(new CustomEvent(eventName, {detail: data}))
+    }
+    _renderAction (actionProperty, user, props) {
+      const { active, applicable, eventData, eventName, hint, icon, iconActive, show } = config[actionProperty]
+
+      const propertyValue = props[actionProperty]
+      const isActive = active(user)
+      const isApplicable = propertyValue && applicable(user)
+      const hintValue = hint(user, propertyValue)
+      const visible = propertyValue || show(user)
+
+      const actionClassNames = classString({action: true, applicable: isApplicable, disabled: propertyValue && !isApplicable})
+      const actionStyles = `background-image: url('${isActive ? iconActive : icon}');`
+      const clickHandler = () => this._handleActionClick(eventName, eventData(user))
+
+      const popover = html`<div class="popover">${hintValue}</div>`
+
+      return visible
+        ? html`
+          <div
+            class$="${actionClassNames}"
+            style="${actionStyles}"
+            on-click="${isApplicable ? clickHandler : null}"
+          >
+            ${popover}
+          </div>
+        `
+        : null
+    }
+    _renderActionsList (user, props) {
+      return html`
+        <div class="actions">
+          ${repeat(Object.keys(config), (actionProperty) => this._renderAction(actionProperty, user, props))}
+        </div>
+      `
+    }
+    _renderListItem (user, props) {
+      return html`
+        <div class="item">
+          ${this._renderUserInfo(user, props)}
+          ${this._renderActionsList(user, props)}
+        </div>
+      `
+    }
   }
 }
 
@@ -246,11 +284,12 @@ function withGroup (baseClass, config) {
 }
 
 export const mixins = {
+  withActions,
+  withFilter,
+  withGroup,
+  withSort,
   withStyle,
   withStyleLink,
-  withFilter,
-  withSort,
-  withGroup
 }
 
 export const UserList = withStyle(UserListElement, style)
